3aae2df6d54f208a5110cbe9993eae2b702a0804
Sergi Hurtado <sergi_h8@hotmail.com>
2018-07-17 11:58:01 +0200

    First commit

---
+++ app/src/main/java/com/example/shurtado/kotlintest/Fragment.kt
@@ -0,0 +1,8 @@
+package com.example.shurtado.kotlintest
+
+class Fragment
+{
+
+
+
+}
---
+++ app/src/main/java/com/example/shurtado/kotlintest/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.shurtado.kotlintest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import retrofit2.Retrofit
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
